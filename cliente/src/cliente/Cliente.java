11fc62839b434a31583c7f6d478ea7e306e9a304
Stan <stan.deuxie@live.com>
2015-12-21 17:51:55 -0500

    cliente

---
+++ cliente/src/cliente/Cliente.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author david
+ */
+import java.io.*;
+import java.net.*;
+public class Cliente {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+     public static void main(String[] args) throws IOException {
+         
+//        if (args.length != 2) {
+//            System.err.println(
+//                "Usage: java EchoClient <host name> <port number>");
+//            System.exit(1);
+//        }
+// 
+        String hostName = "localhost";
+        int portNumber = 8889;
+ 
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out =
+                new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in =
+                new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn =
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }    // TODO code application logic here
+    }
+    
+
